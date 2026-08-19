e3aead8b33cbac9c7337e0c8e09604f1fa2bee30
Dimas <dimasars79@gmail.com>
2026-08-19 15:30:02 +0700

    feat: add startup script to launch portfolio website and development server

---
+++ start-portfolio.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ====================================================
+echo Starting Dimas Ahmad Noorsaid Portfolio Website...
+echo ====================================================
+start http://localhost:5173
+npm run dev
